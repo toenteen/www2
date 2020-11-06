d6853bd58ade27105cd1a169585394a7251a53fa
Chatchanon63070033 <73678877+Chatchanon63070033@users.noreply.github.com>
2020-11-06 18:41:37 +0700

    Create naruto.php

---
+++ naruto.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <title>Hello</title>
+    <link
+      rel="stylesheet"
+      href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css"
+      integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2"
+      crossorigin="anonymous"
+    />
+  </head>
+  <body>
+    <div class="container">
+      <div class="row">
+        <div class="col">
+          <div class="alert alert-success" role="alert">
+            <div class="card">
+              <img class="card-img-top" src="obito.jpg" />
+              <div class="card-body">
+                <div class="alert alert-success" role="alert">
+                  <h4 class="card-title">Obito Uchiha</h4>
+                </div>
+                <p class="card-text">
+                  Obito is a very handsome and good.
+                </p>
+                <p>
+                  <a href="#" class="btn btn-success">Read more</a>
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col">
+          <div class="alert alert-info" role="alert">
+            <div class="card">
+              <img class="card-img-top" src="doh5.webp" />
+              <div class="card-body">
+                <div class="alert alert-info" role="alert">
+                  <h4 class="card-title">Itachi Uchiha</h4>
+                </div>
+                <p class="card-text">
+                  Itachi is so cute
+                </p>
+                <p>
+                  <a href="#" class="btn btn-info">Read more</a>
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col">
+          <div class="alert alert-danger" role="alert">
+            <div class="card">
+              <img class="card-img-top" src="doh6.jpg" />
+              <div class="card-body">
+                <div class="alert alert-danger" role="alert">
+                  <h4 class="card-title">Naruto Uzumaki</h4>
+                </div>
+                <p class="card-text">
+                  Naruto is Honorable.
+                </p>
+                <p>
+                  <a href="#" class="btn btn-danger">Read more</a>
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- Close container -->
+  </body>
+</html>
